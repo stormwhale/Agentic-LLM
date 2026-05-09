d457e4f02fbdecc398c9b9e692d68f8c94f5faa0
stormwhale <178974193+stormwhale@users.noreply.github.com>
2026-05-09 13:30:52 -0400

    Delete StockMarketChatbot(SMC)/SMC_v0.4 directory

--- StockMarketChatbot(SMC)/SMC_v0.4/run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Installing dependencies...
-py -m pip install -r requirements.txt --quiet
-echo.
-
-echo Starting Stock Market Chatbot...
-echo.
-py -m streamlit run smc_0_4_release.py
-pause
